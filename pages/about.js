0c92b2f3f6bcfc0fab7df3f032f6488cdccf5819
Ariful Islam <iariful.info@gmail.com>
2021-03-28 20:00:21 +0600

    fetch data from json placeholder

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {developer__list, developer} from '../styles/About.module.css'
 
-const about = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { users: data}
+    }
+}
+
+const about = ({users}) => {
     return (
         <div>
-            <h1>This is a About page</h1>
+            <h1 className={developer__list}>Here is Our Developer</h1>
+            {
+                users.map (user => (
+                    <div key={user.id}>
+                        <a>
+                            <h3 className={developer}>{user.name}</h3>
+                        </a>
+                    </div>
+                ))
+            }
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import { title, text, btn } from '../styles/Home.module.css'
+import Image from 'next/image'
+
 export default function Home() {
   return (
+    <>
     <div>
       <h1 className = {title}>This is a Homepage</h1>
       <p className = {text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium et dolorem sed voluptatem nulla nam, reiciendis enim distinctio aliquid consequuntur animi in illo iure doloremque non voluptate. Adipisci pariatur odio incidunt earum odit, delectus recusandae doloremque eveniet iure. Optio delectus harum eaque, sint et ut dicta rerum laudantium, inventore eius facere. A iure itaque quis accusantium aliquam enim delectus rerum in velit. Pariatur, magnam blanditiis soluta sit repudiandae recusandae hic officia vero earum harum voluptatem. Accusantium, id quod voluptas beatae earum incidunt quasi sapiente, soluta pariatur eos praesentium. Molestiae inventore dolorem error eum iusto odit est aliquam, aperiam minus quis.</p>
       <button className={btn}>Explore more</button>
-    </div>
+        <Image src="/img/code.png" width={300} height={300} alt="code is my passion" />
+      </div>
+    
+    </>
   )
 }
